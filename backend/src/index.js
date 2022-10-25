f85629313f5f8069fa7acf3cfe02741316a97fa4
emivnajera <emivelasquez2000@hotmail.com>
2022-10-24 19:18:23 -0600

    se agrega el endopint inicial.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+//settings
+app.set('port', process.env.PORT || 8080)
 
+//middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extenden: false}))
+app.use(express.json())
 
-app.listen(3000, ()=>{
-    console.log('Server on port 3000');
+
+//routes
+app.get('/', (req, res)=>{
+    res.send('Hello World');
+})
+
+//starting server
+app.listen(app.get('port'), ()=>{
+    console.log(`Server on port ${app.get('port')}`)
 })
